3747c5abfbdf046dd0256729c72ec612a9281755
jesse10930 <ronda.jesse@gmail.com>
2019-06-29 14:54:45 -0400

    Updates

---
+++ precache-manifest.c9e0fb5069311472430effd3efd994ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2a748b8689f731107cfe",
+    "url": "/roboFriends/static/js/runtime~main.2a748b86.js"
+  },
+  {
+    "revision": "ef865617c5fa55f982f8",
+    "url": "/roboFriends/static/js/main.ef865617.chunk.js"
+  },
+  {
+    "revision": "51ff63abca9ea759d6e5",
+    "url": "/roboFriends/static/js/2.51ff63ab.chunk.js"
+  },
+  {
+    "revision": "ef865617c5fa55f982f8",
+    "url": "/roboFriends/static/css/main.437b0d17.chunk.css"
+  },
+  {
+    "revision": "51ff63abca9ea759d6e5",
+    "url": "/roboFriends/static/css/2.72c93c2b.chunk.css"
+  },
+  {
+    "revision": "8dde2d562410764920cc949ca634dec9",
+    "url": "/roboFriends/index.html"
+  }
+];
